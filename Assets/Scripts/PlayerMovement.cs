a4b028ca9478055c00de7b11987b955e7e3c8c5e
Ruan Emanuell <ruanvitorcom540@gmail.com>
2023-01-20 13:16:24 -0300

    feat: adding text

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Rigidbody rb;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(Input.GetKey("w")){
+            rb.AddForce(0, 0, 8000*Time.deltaTime);
+        }
+        if(Input.GetKey("s")){
+            rb.AddForce(0, 0, -8000*Time.deltaTime);
+        }
+        if(Input.GetKey("d")){
+            rb.AddForce(4000*Time.deltaTime, 0, 0);
+        }
+        if(Input.GetKey("a")){
+            rb.AddForce(-4000*Time.deltaTime, 0, 0);
+        }
+    }
+}
