f4774125c87d2c13588c8d5dfbe93dae83a90462
Ruan Emanuell <ruanvitorcom540@gmail.com>
2023-01-22 20:37:19 -0300

    feat: finishing the game

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+
+    bool gameEnd=false;
+    public float restartDelay=1f;
+
+    public GameObject completeLevelUI;
+
+    public void FinishLevel(){
+        completeLevelUI.SetActive(true);
+
+    }
+
+    public void GameOver(){
+        if(gameEnd==false){
+            Debug.Log("Game Over");
+            gameEnd=true;
+            Invoke("Restart", 1f);
+        }
+    }
+
+    public void Restart(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        gameEnd=false;
+    }
+
+}
